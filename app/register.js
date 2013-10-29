e8ae4cf93d8324e70b7d0c5c5c189dc2031cc082
Jaco Joubert <joubert.jaco@gmail.com>
2013-10-29 15:11:30 -0400

    add dropbutton component. Missing ability to display extended list of actions.

--- app/register.js
+++ app/register.js
@@ -1,5 +1,6 @@
 EmberUI.Register = Ember.Mixin.create({
   setupComponents: function() {
     this.EuiButtonComponent = EmberUI.EuiButtonComponent;
+    this.EuiDropbuttonComponent = EmberUI.EuiDropbuttonComponent;
   }.on('init')
 });
--- app/scripts/components/EuiButtonComponent.js
+++ app/scripts/components/EuiButtonComponent.js
@@ -1,7 +1,17 @@
 EmberUI.EuiButtonComponent = Ember.Component.extend({
   tagName: 'button',
-  classNameBindings: [':eui-button', 'computedSize', 'computedType', 'loading:eui-loading', 'icon:eui-icon'],
+  classNameBindings: [':eui-button', 'computedSize', 'computedType', 'loading:eui-loading', 'icon:eui-icon', 'label::eui-no-label', 'class'],
   attributeBindings: ['isDisabled:disabled'],
+
+  label: null,
+  type: null,
+  size: null,
+  icon: null,
+  trailingIcon: null,
+  loading: null,
+  disabled: null,
+  action: null,
+  class: null,
 
   computedSize: function() {
     return 'eui-' + (this.get('size') || 'medium');
@@ -13,5 +23,9 @@ EmberUI.EuiButtonComponent = Ember.Component.extend({
 
   isDisabled: function() {
     if (this.get('disabled') || this.get('loading')) return true;
-  }.property('disabled', 'loading')
+  }.property('disabled', 'loading'),
+
+  click: function() {
+    this.sendAction();
+  }
 });
