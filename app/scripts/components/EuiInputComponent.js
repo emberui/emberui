53b868b0862a7d978066d27531951e19af54c99a
Jaco Joubert <joubert.jaco@gmail.com>
2013-11-13 16:19:26 -0500

    emberui input: use ember input for input, css styling, sensible defaults for width

--- app/scripts/components/EuiInputComponent.js
+++ app/scripts/components/EuiInputComponent.js
@@ -1,10 +1,17 @@
 EmberUI.EuiInputComponent = Ember.Component.extend({
   tagName: 'div',
   classNameBindings: [':eui-input', 'computedSize', 'computedStyle', 'class'],
+  attributeBindings: ['computedWidth:style'],
 
   style: null,
   size: null,
+  width: null,
+  name: null,
+  pattern: null,
   disabled: null,
+  maxlength: null,
+  tabindex: null,
+  placeholder: null,
   value: null,
   class: null,
 
@@ -14,5 +21,10 @@ EmberUI.EuiInputComponent = Ember.Component.extend({
 
   computedStyle: function() {
     return 'eui-' + (this.get('type') || 'default');
-  }.property('type')
+  }.property('type'),
+
+  computedWidth: function() {
+    var widths = {'tiny': '100px', 'small': '150px', 'medium': '200px', 'large': '250px'};
+    return 'width: ' + (this.get('width') || widths[this.get('size')] || widths['medium']);
+  }.property('type', 'size')
 });
