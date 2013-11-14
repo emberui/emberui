2a2676722aa85e108eb7c36caec7391c9d23c6d4
Jaco Joubert <joubert.jaco@gmail.com>
2013-11-14 11:22:41 -0500

    eui-input: add error handling

--- app/scripts/components/EuiInputComponent.js
+++ app/scripts/components/EuiInputComponent.js
@@ -1,6 +1,6 @@
 EmberUI.EuiInputComponent = Ember.Component.extend({
   tagName: 'div',
-  classNameBindings: [':eui-input', 'computedSize', 'computedStyle', 'class'],
+  classNameBindings: [':eui-input', 'computedSize', 'computedStyle', 'class', 'computedErrorState:eui-error'],
   attributeBindings: ['computedWidth:style'],
 
   style: null,
@@ -14,6 +14,9 @@ EmberUI.EuiInputComponent = Ember.Component.extend({
   placeholder: null,
   value: null,
   class: null,
+  required: null,
+  hasError: null,
+  errorMessage: null,
 
   computedSize: function() {
     return 'eui-' + (this.get('size') || 'medium');
@@ -26,5 +29,41 @@ EmberUI.EuiInputComponent = Ember.Component.extend({
   computedWidth: function() {
     var widths = {'tiny': '100px', 'small': '150px', 'medium': '200px', 'large': '250px'};
     return 'width: ' + (this.get('width') || widths[this.get('size')] || widths['medium']);
-  }.property('style', 'size')
+  }.property('style', 'size'),
+
+
+  computedErrorState: null,
+  computedErrorMessage: null,
+
+  validateField: function(type) {
+    var hasError = this.get('hasError');
+    var errorMessage = this.get('errorMessage');
+    var required = this.get('required');
+    var value = this.get('value');
+
+    if ( type === 'onload' && value === null) return;
+
+    if (hasError || errorMessage || (required && !value)) {
+      this.set('computedErrorState', true);
+
+      if (hasError && typeof(hasError) !== 'boolean' ) {
+        this.set('computedErrorMessage', hasError);
+
+      } else if (errorMessage) {
+        this.set('computedErrorMessage', errorMessage);
+      }
+
+    } else {
+      this.set('computedErrorState', false);
+      this.set('computedErrorMessage', null);
+    }
+  },
+
+  focusOut: function() {
+    this.validateField();
+  },
+
+  validateOnLoad: function() {
+    this.validateField('onload');
+  }.on('init')
 });
