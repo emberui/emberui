24bc75a891fb22a06fdfba7d29667db7c341fdf1
Jaco Joubert <joubert.jaco@gmail.com>
2014-01-15 17:17:15 -0500

    basic framing of documentation site

--- app/router.js
+++ app/router.js
@@ -1,6 +1,7 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function() {
+  this.route("documentation", { path: "/documentation" });
 });
 
 export default Router;
