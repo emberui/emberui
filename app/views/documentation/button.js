b6cc6efc0e8fe23a73cb533746798cabc7c99df0
Jaco Joubert <joubert.jaco@gmail.com>
2014-01-16 13:39:01 -0500

    add sample code to eui-button docs via view

---
+++ app/views/documentation/button.js
@@ -0,0 +1,6 @@
+export default Ember.View.extend({
+  exampleLabel: '{{eui-button label="Save Changes"}}',
+  exampleSize: '{{eui-button label="Save Changes" size="tiny"}} \n{{eui-button label="Save Changes" size="small"}} \n{{eui-button label="Save Changes" size="medium"}} \n{{eui-button label="Save Changes" size="large"}}',
+  exampleStyle: '{{eui-button label="Save Changes" style="secondary"}} \n{{eui-button label="Save Changes" style="primary"}}',
+  exampleIcon: '{{eui-button icon="fa fa-cog"}} \n{{eui-button label="Settings" icon="fa fa-cog"}}'
+});
