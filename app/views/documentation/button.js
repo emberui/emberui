f6ae02e83f1ee53e1344f7b36c86f5422aa179c2
Jaco Joubert <joubert.jaco@gmail.com>
2014-01-16 14:03:42 -0500

    polish to documentation style

--- app/views/documentation/button.js
+++ app/views/documentation/button.js
@@ -2,5 +2,5 @@ export default Ember.View.extend({
   exampleLabel: "{{eui-button label='Save Changes'}}",
   exampleSize: "{{eui-button label='Save Changes' size='tiny'}} \n{{eui-button label='Save Changes' size='small'}} \n{{eui-button label='Save Changes' size='medium'}} \n{{eui-button label='Save Changes' size='large'}}",
   exampleStyle: "{{eui-button label='Save Changes' style='secondary'}} \n{{eui-button label='Save Changes' style='primary'}}",
-  exampleIcon: "{{eui-button icon='fa fa-cog'}} \n{{eui-button label='Settings' icon='fa fa-cog'}}"
+  exampleIcon: "{{eui-button label='Settings' icon='fa fa-cog'}} \n{{eui-button icon='fa fa-cog'}}"
 });
