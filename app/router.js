3622b363a67bccd33d87c90bea56121a85aa6ef6
Jaco Joubert <joubert.jaco@gmail.com>
2014-01-16 16:47:06 -0500

    add parameter list to top of docs, use history API to manage urls so parameter list and use #

--- app/router.js
+++ app/router.js
@@ -1,5 +1,9 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
+Router.reopen({
+  location: 'history'
+});
+
 Router.map(function() {
   this.resource("documentation", { path: "/documentation" }, function() {
     this.route('button', { path: '/button' });
