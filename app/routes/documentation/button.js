967b90a3a175a5ee0d6df206479ce2960c51a717
Jaco Joubert <joubert.jaco@gmail.com>
2014-01-17 12:48:15 -0500

    additional docs for eui-button

---
+++ app/routes/documentation/button.js
@@ -0,0 +1,11 @@
+export default Ember.Route.extend({
+  actions: {
+    switchToLoading: function(controller) {
+      this.get("controller").set('isLoading', true);
+    },
+
+    switchToDisabled: function(controller) {
+      this.get("controller").set('isDisabled', true);
+    }
+  }
+});
--- app/views/documentation/button.js
+++ app/views/documentation/button.js
@@ -2,5 +2,9 @@ export default Ember.View.extend({
   exampleLabel: "{{eui-button label='Save Changes'}}",
   exampleSize: "{{eui-button label='Save Changes' size='tiny'}} \n{{eui-button label='Save Changes' size='small'}} \n{{eui-button label='Save Changes' size='medium'}} \n{{eui-button label='Save Changes' size='large'}}",
   exampleStyle: "{{eui-button label='Save Changes' style='secondary'}} \n{{eui-button label='Save Changes' style='primary'}}",
-  exampleIcon: "{{eui-button label='Settings' icon='fa fa-cog'}} \n{{eui-button icon='fa fa-cog'}}"
+  exampleIcon: "{{eui-button label='Settings' icon='fa fa-cog'}} \n{{eui-button icon='fa fa-cog'}}",
+  exampleDisabled_A: "{{eui-button label='Save Changes' disabled=true}} \n{{eui-button label='Click to Disable' disabledBinding=isDisabled action='switchToDisabled'}}",
+  exampleDisabled_B: "actions: { \n  switchToDisabled: function(controller) { \n    this.get('controller').set('isDisabled', true);\n  } \n}",
+  exampleLoading_A: "{{eui-button label='Save Changed' loading=true}} \n{{eui-button label='Click to Load' loadingBinding=isLoading action='switchToLoading'}}",
+  exampleLoading_B: "actions: { \n  switchToLoading: function(controller) { \n    this.get('controller').set('isLoading', true);\n  } \n}"
 });
