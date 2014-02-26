1b24d8d79358f27bb311d32aa61de820221cdcae
Zach Aysan <zachaysan@gmail.com>
2014-02-26 13:21:20 -0500

    Make packager for submodule

---
+++ package.sh
@@ -0,0 +1,20 @@
+COMMIT_MSG=$(git config --list | grep "user.name" | cut -d = -f 2) &&
+COMMIT_MSG=$COMMIT_MSG": "                                         &&
+COMMIT_MSG=$COMMIT_MSG$(git log -1 --oneline)                      &&
+echo $COMMIT_MSG                                                   &&
+
+grunt dist                        &&
+WEBSITE="website"                 &&
+cd $WEBSITE                       &&
+
+git pull --rebase                 &&
+rm -rf *                          &&
+git rm *                          &&
+cp -r ../dist/* ./                &&
+git add .                         &&
+git commit -S -am "$COMMIT_MSG"   &&
+git push origin HEAD --force      &&
+cd ..                             &&
+git commit -S -o website -m "Update website with current changes" &&
+
+echo "Successfully built package"
