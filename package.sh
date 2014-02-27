b965c636a0e129a9fc8b1599a55708fe442ef81f
Zach Aysan <zachaysan@gmail.com>
2014-02-27 11:38:39 -0500

    Make 404.html a cp of index.html

--- package.sh
+++ package.sh
@@ -11,14 +11,12 @@ cd $WEBSITE                       &&
 
 git pull                          &&
 cp CNAME /tmp/CNAME               &&
-cp 404.html /tmp/404.html         &&
 rm -rf *                          &&
 git rm *                          &&
 cp /tmp/CNAME CNAME               &&
-cp /tmp/404.html 404.html         &&
 git add CNAME                     &&
-git add 404.html                  &&
 cp -r ../dist/* ./                &&
+cp index.html 404.html            &&
 git add .                         &&
 git commit -am "$COMMIT_MSG"      &&
 git push origin HEAD --force      &&
