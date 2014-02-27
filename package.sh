c8b663e7a63eea3787c286eddc52c33c81f6e417
Zach Aysan <zachaysan@gmail.com>
2014-02-27 11:09:03 -0500

    Use git@... naming

--- package.sh
+++ package.sh
@@ -7,9 +7,17 @@ grunt dist                        &&
 WEBSITE="website"                 &&
 cd $WEBSITE                       &&
 
+
+
 git pull --rebase                 &&
+cp CNAME /tmp/CNAME               &&
+cp 404.html /tmp/404.html         &&
 rm -rf *                          &&
+cp /tmp/CNAME CNAME               &&
+cp /tmp/404.html 404.html         &&
 git rm *                          &&
+git add CNAME                     &&
+git add 404.html                  &&
 cp -r ../dist/* ./                &&
 git add .                         &&
 git commit -am "$COMMIT_MSG"      &&
