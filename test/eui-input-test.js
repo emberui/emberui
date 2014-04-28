dbfaae1b07b6b2632b401e9cf63e0f37dbc7678a
Mattia Gheda <ghedamat@gmail.com>
2014-04-28 11:59:47 -0400

    text-support mixin should use on('didInsertElement')
    this let's users of the mixin implement their on version
    
    I took the chance to add a simple test for this use case.
    It turns out it's probably a good idea for us to export also the
    templates as I'd rather test against the real templates than using a
    fake version.
    
    Thoughts? /cc @elucid @jacojoubert

--- test/eui-input-test.js
+++ test/eui-input-test.js
@@ -6,3 +6,14 @@ test('starts with errorState null', function() {
   this.append();
   equal(input.get('errorState'), null);
 });
+
+test('inputId is stored on didInsertElement', function() {
+  expect(2);
+  var input = this.subject({
+    value: 'bonk',
+    error: true
+  });
+  ok(!input.get('inputId'), 'inputId is not set until append');
+  this.append()
+  ok(input.get('inputId'), 'inputId is set after append');
+});
