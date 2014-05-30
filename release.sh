98dc29f7694e3549f8c9cd455527e3cfa715d440
Mattia Gheda <ghedamat@gmail.com>
2014-05-30 14:22:11 -0400

    release.sh updates also package.json

--- release.sh
+++ release.sh
@@ -19,12 +19,16 @@ fi
 # update bower.json
 sed -i -r 's/^  "version": "[0-9]+\.[0-9]+\.[0-9]+",/  "version": "'$VERSION'",/g' bower.json
 
+# update package.json
+sed -i -r 's/^  "version": "[0-9]+\.[0-9]+\.[0-9]+",/  "version": "'$VERSION'",/g' package.json
+
 # update lib file
 sed -i -r 's/^Ember.libraries.register\("EmberUI", "[0-9]+\.[0-9]+\.[0-9]+"\);/Ember.libraries.register("EmberUI", "'$VERSION'");/g' lib/emberui.js
 
 rm -rf dist && broccoli build dist
 git add -A dist
 git add bower.json
+git add package.json
 git add lib/emberui.js
 git commit -m "Version bump $VERSION"
 git tag -a "v$VERSION" -m "v$VERSION"
