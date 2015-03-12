d2f2c243aed3b0cf46b6ea65cb5fc992f4f31088
Jaco Joubert <jacojoubert@Jacos-iMac.local>
2015-03-12 12:46:24 -0400

    add brocolli-sass, velocity, and list-view to blueprint

--- blueprints/ember-cli-emberui/index.js
+++ blueprints/ember-cli-emberui/index.js
@@ -2,10 +2,16 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall: function() {
+    this.addPackagesToProject([
+      { name: 'broccoli-sass' }
+    ]);
+
+    this.addAddonToProject('ember-cli-velocity');
+    this.addAddonToProject('ember-cli-list-view');
+
     return this.addBowerPackagesToProject([
       { name: 'moment' },
-      { name: 'ember-cli-list-view' },
       { name: 'twix'}
-    ])
+    ]);
   }
 };
