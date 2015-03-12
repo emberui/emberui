75098df5d6578169cbb033ee3ffa4e90502d4224
Jaco Joubert <jacojoubert@Jacos-iMac.local>
2015-03-12 13:38:59 -0400

    use promises when using afterInstall hook

--- blueprints/ember-cli-emberui/index.js
+++ blueprints/ember-cli-emberui/index.js
@@ -2,16 +2,22 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
   afterInstall: function() {
-    this.addPackagesToProject([
+    return this.addPackagesToProject([
+      // TODO: remove this because it forces the consuming user to use app.scss
       { name: 'broccoli-sass' }
-    ]);
 
-    this.addAddonToProject('ember-cli-velocity');
-    this.addAddonToProject('ember-cli-list-view');
+    ]).then(function() {
+      return this.addBowerPackagesToProject([
+        { name: 'moment' },
+        { name: 'twix'}
+      ])
 
-    return this.addBowerPackagesToProject([
-      { name: 'moment' },
-      { name: 'twix'}
-    ]);
+    }.bind(this)).then(function() {
+      return this.addAddonToProject('ember-cli-velocity')
+
+    }.bind(this)).then(function() {
+      return this.addAddonToProject('ember-cli-list-view')
+
+    }.bind(this))
   }
 };
