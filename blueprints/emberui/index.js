76bd6e353709798672fa0331bcd3ae2dae471bd3
Jaco Joubert <joubert.jaco@gmail.com>
2016-02-12 15:07:11 -0500

    user ember addon to install moment instead of bower

--- blueprints/emberui/index.js
+++ blueprints/emberui/index.js
@@ -3,14 +3,16 @@ module.exports = {
 
   afterInstall: function() {
     return this.addBowerPackagesToProject([
-      { name: 'moment' },
       { name: 'twix'}
 
     ]).then(function() {
-      return this.addAddonToProject('ember-cli-sass')
+      return this.addAddonToProject('ember-cli-moment-shim');
 
     }.bind(this)).then(function() {
-      return this.addAddonToProject('ember-cli-velocity')
+      return this.addAddonToProject('ember-cli-sass');
+
+    }.bind(this)).then(function() {
+      return this.addAddonToProject('ember-cli-velocity');
 
     }.bind(this));
   }
