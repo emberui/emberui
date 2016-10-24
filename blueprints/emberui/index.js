d1ca768465545793d1f7462e839cc41838bfc2cb
Jaco Joubert <joubert.jaco@gmail.com>
2016-10-24 14:58:30 -0400

    Ensure Twix is added to parent app.

---
+++ blueprints/emberui/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
+
+  afterInstall: function() {
+    return this.addBowerPackageToProject('twix');
+  }
+};
