046ef4d7e6a30e27f9994bb14c52d95a671513d1
Jaco Joubert <joubert.jaco@gmail.com>
2019-10-17 09:39:49 -0400

    Remove twix

--- addon/components/eui-selectdate.js
+++ addon/components/eui-selectdate.js
@@ -3,7 +3,6 @@ import disabledSupport from '../mixins/disabled-support';
 import widthSupport from '../mixins/width-support';
 import errorSupport from '../mixins/error-support';
 import moment from 'moment';
-import twix from 'twix';
 
 export default Ember.Component.extend(disabledSupport, errorSupport, widthSupport, {
   classNameBindings: ['isDisabled:eui-disabled', 'class'],
@@ -24,11 +23,7 @@ export default Ember.Component.extend(disabledSupport, errorSupport, widthSuppor
 
   showCalendarWindow: false,
 
-  formatting: {
-    yearFormat: "YYYY",
-    monthFormat: "MMMM",
-    dayFormat: "D"
-  },
+  formatting: "MMMM D, YYYY",
 
   attachment: 'center center',
   targetAttachment: 'center center',
@@ -276,7 +271,7 @@ export default Ember.Component.extend(disabledSupport, errorSupport, widthSuppor
       return;
     }
 
-    return date.twix(date, true).format(formatting);
+    return date.format(formatting);
   },
 
   formatDateRange(startDate, endDate, formatting) {
@@ -286,15 +281,15 @@ export default Ember.Component.extend(disabledSupport, errorSupport, widthSuppor
 
     // No end date is selected so show partial date
     if (!endDate) {
-      return startDate.twix(startDate, true).format(formatting) + ' -';
+      return startDate.format(formatting) + ' -';
     }
 
     // Full date range
     if (startDate && endDate) {
       if (endDate.isBefore(startDate)) {
-        return endDate.twix(startDate, true).format(formatting);
+        return endDate.format(formatting) + ' - ' + startDate.format(formatting);
       } else {
-        return startDate.twix(endDate, true).format(formatting);
+        return startDate.format(formatting) + ' - ' + endDate.format(formatting);
       }
     }
   },
